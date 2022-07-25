9a339a5224a632f318136eea25b16053ea60f5bd
jojooah <my_sg1@naver.com>
2022-07-25 15:40:04 +0900

    gitignore파일 변경

---
+++ app/bin/www.js
@@ -0,0 +1,7 @@
+const app=require("../app")
+
+const PORT=3000;
+
+app.listen(3000,()=>{
+    console.log('서버 가동');
+});
---
+++ app/src/routes/home/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const express=require("express");
+const app=express();
+const router=express.Router();
+
+app.get("/",(req,res)=>{
+    res.render("home/index");
+});
+
+
+app.get("/login",(req,res)=>{
+    res.render("home/login");
+})
+
+module.exports=router;
